a2ff92c05b66d449bf0f00adb2719a03e993eeb1
Saikrishna <saikrishnakeerti@gmail.com>
2018-10-10 12:22:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,127 @@
 var sipStack;
-var registerSession;
-var sipStack;
-var registerSession;
+var registerSession, callSession;
+var oConfigCall;
 
 function callEvent() {
-  callSession = sipStack.newSession("call-audiovideo", {
+  oConfigCall = {
     audio_remote: document.getElementById("audio-remote"),
-    events_listener: { events: "*", listener: eventsListener }
-  });
-  callSession.call(document.getElementById("extension").value);
-
-  document.getElementById("call").disabled = true;
-  document.getElementById("hangup").disabled = false;
+    screencast_window_id: 0x00000000,
+    bandwidth: { audio: undefined, video: undefined },
+    events_listener: { events: "*", listener: eventSession },
+    sip_caps: [
+      { name: "+g.oma.sip-im" },
+      { name: "language", value: '"en,fr"' }
+    ]
+  };
+  if (
+    sipStack &&
+    !callSession && 
+    !tsk_string_is_null_or_empty(document.getElementById("extension").value)
+  ) {
+    document.getElementById("call").disabled = true;
+    document.getElementById("hangup").disabled = false;
+    callSession = sipStack.newSession("call-audio", oConfigCall);
+    // make call
+    if (callSession.call(document.getElementById("extension").value) != 0) {
+      callSession = null;
+      document.getElementById("showCallStatus") = "Failed to make call";
+      document.getElementById("call").disabled = false;
+      document.getElementById("hangup").disabled = true;
+      return;
+    }
+    document.getElementById("showCallStatus").innerHTML = "In Call";
+  } else if (callSession) {
+    document.getElementById("showCallStatus").innerHTML = "In Call";
+    document.getElementById("call").disabled = true;
+    callSession.accept(oConfigCall);
+  }
 }
 
-function hangupEvent() {
+function hangUpEvent() {
+  try {
+  if (callSession) {
+    callSession.hangup({
+      events_listener: { events: "*", listener: eventSession }
+    });
+  }
   document.getElementById("hangup").disabled = true;
   document.getElementById("call").disabled = false;
+  }
+  catch(e){
+    console.log("call cancelled");
+    window.location.reload();
+  }
 }
 
-var acceptCall = function(e) {
-  e.newSession.accept(); // e.newSession.reject() to reject the call
-};
-
 var eventsListener = function(e) {
-  console.info("session event = " + e.type);
+  console.info("stack event = " + e.type);
+  console.info("stack description = " + e.description);
+
   if (e.type == "started") {
     login();
   } else if (e.type == "i_new_call") {
-    // incoming audio/video call
-    acceptCall(e);
-  } else if (e.type == "connected" && e.session == registerSession) {
-    callEvent();
+    // incoming audio call
+    if (callSession) {
+      // do not accept the incoming call if we're already 'in call'
+      e.newSession.hangup(); 
+    }
+    else {
+      callSession = e.newSession;
+      // start listening for events
+      callSession.setConfiguration(
+      audio_remote = document.getElementById("audio-remote"),
+      screencast_window_id =  0x00000000,
+      bandwidth = { audio: undefined, video: undefined },
+      events_listener = { events: "*", listener: eventSession },
+      sip_caps = [
+        { name: "+g.oma.sip-im" },
+        { name: "language", value: '"en,fr"' }
+      ]);
+      document.getElementById("call").innerHTML = "Answer";
+      document.getElementById("hangup").disabled = false;
+      document.getElementById("showCallStatus").innerHTML = "Incoming Call";
+    }
+  } else if(e.description == "Failed to connet to the server") {
+    document.getElementById("showConnectionStatus").innerHTML = e.description;
   }
 };
 
+var eventSession = function(e) {
+  console.info("session event = " + e.type);
+  console.log("session description = " + e.description);
+  if(e.description == "Forbidden") {
+    document.getElementById("showConnectionStatus").innerHTML =
+      "Forbidden (Incorrect password)";
+  } else if (
+    e.description == "Call terminated" ||
+    e.description == "Request Terminated" ||
+    e.description == "Call Rejected"
+  ) {
+    callSession = null;
+    registerSession = null;
+    document.getElementById("showCallStatus").innerHTML = "";
+    document.getElementById("hangup").disabled = true;
+    document.getElementById("call").disabled = false;
+    document.getElementById("call").innerHTML = "Call";
+
+  } else if (e.description == "Busy Here" ){
+    document.getElementById("showCallStatus").innerHTML = "User Busy";
+    callSession = null;
+    registerSession = null;
+    document.getElementById("hangup").disabled = true;
+    document.getElementById("call").disabled = false;
+    document.getElementById("call").innerHTML = "Call";
+  }
+}
+
 function connect() {
   sipStack = new SIPml.Stack({
-    realm: "172.16.16.45", // mandatory: domain name
+    realm: "172.16.16.000", // mandatory: domain name
     impi: "sipML5", // mandatory: authorization name (IMS Private Identity)
     impu: "sip:sipML5@172.16.16.45:8089", // mandatory: valid SIP Uri (IMS Public Identity)
     password: "test123", // optional
     display_name: "sk", // optional
-    websocket_proxy_url: 'wss://172.16.16.45:8089/ws',
+    websocket_proxy_url: "wss://172.16.16.45:8089/ws",
     events_listener: { events: "*", listener: eventsListener },
     sip_headers: [
       { name: "User-Agent", value: "IM-client/OMA1.0 sipML5-v1.0.0.0" },
@@ -54,7 +133,7 @@ function connect() {
 
 var login = function() {
   registerSession = sipStack.newSession("register", {
-    events_listener: { events: "*", listener: eventsListener }
+    events_listener: { events: "*", listener: eventSession }
   });
   registerSession.register();
 };
